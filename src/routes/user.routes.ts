6c1b7042557defa923f6a2b056e0551765cc914a
erikarosah <erikarosamg@gmail.com>
2023-12-06 11:59:19 -0300

    deleted files

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,19 +1,13 @@
 import { Router } from "express";
 import  UserController  from "../controllers/UserController";
-import UserAvatarController from "../controllers/UserAvatarController";
-import { uploadConfig } from "../configs/upload";
-import multer from "multer";
+
 import ensureAuthenticated from "../middleware/ensureAuthenticated";
 
 const userRoutes = Router();
 const userController = new UserController();
-const userAvatarController = new UserAvatarController();
-
-const upload = multer(uploadConfig.MULTER);
 
 userRoutes.post('/', userController.create);
 userRoutes.put('/', ensureAuthenticated,  userController.update);
-userRoutes.patch('/avatar', ensureAuthenticated,  upload.single("avatar"), userAvatarController.update);
 
 
 export default userRoutes;
